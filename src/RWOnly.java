5f88a25faab19ee951e78fe4e1396e46a384d939
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-14 15:44:40 +0530

    Update RWOnly.java

--- src/RWOnly.java
+++ src/RWOnly.java
@@ -1,1 +1,15 @@
+package encapsulation;
 
+public class RWOnly {
+    private String name;
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+}
