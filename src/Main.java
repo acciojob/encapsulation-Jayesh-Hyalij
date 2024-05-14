6dd1561f862af726cd0c5b4b14e13ffc049a7d33
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-14 15:45:16 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,14 @@
+package encapsulation;
 
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "Jayesh"; // This will cause a compile-time error because name is a private member variable
+
+        // Instead, we can use the setter method to set the value of name
+        obj.setName("Jayesh");
+
+        // And we can use the getter method to print the value of name
+        System.out.println(obj.getName());
+    }
+}
